cb8e1ccf0627e7e529eac468abaac94d2d7e76bc
ridhomujizat <ridhomujizat@gmail.com>
2020-12-20 22:59:09 +0700

    Home Page

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { combineReducers, createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import authReducer from "../features/Auth/reducer";
+import productReducer from "../features/Product/reducer";
 
 const composerEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducers = combineReducers({
   auth: authReducer,
+  products: productReducer,
 });
 
 const store = createStore(
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,117 @@
-import React from "react";
+import { React, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  SideNav,
+  LayoutSidebar,
+  Responsive,
+  CardProduct,
+  Pagination,
+  InputText,
+  Pill,
+} from "upkit";
+import BounceLoader from "react-spinners/BounceLoader";
+import TopBar from "../../components/TopBar";
 import Menus from "./menus";
-import { SideNav, LayoutSidebar } from "upkit";
+import { config } from "../../config";
+import { tags } from "./tags";
+
+import {
+  fetchProduct,
+  setPage,
+  goToNextPage,
+  gotoPrevPage,
+  setKeyword,
+  setCategory,
+  toggleTag,
+} from "../../features/Product/actions";
 
 export default function Home() {
+  let dispatch = useDispatch();
+  let products = useSelector((state) => state.products);
+
+  useEffect(() => {
+    dispatch(fetchProduct());
+  }, [
+    dispatch,
+    products.currentPage,
+    products.keyword,
+    products.category,
+    products.tags,
+  ]);
+
   return (
     <div>
       <LayoutSidebar
-        sidebar={<SideNav items={Menus} verticalAlign="top" />}
+        sidebar={
+          <SideNav
+            items={Menus}
+            verticalAlign="top"
+            onChange={(category) => dispatch(setCategory(category))}
+            active={products.category}
+          />
+        }
         content={
           <div className="md:flex md:flex-row-reverse w-full mr-5 h-full min-h-screen">
             <div className="w-full md:w-3/4 pl-5 pb-10">
-              Konten utama di sini
+              <TopBar />
+
+              <div className="w-full text-center mb-10 mt-5">
+                <InputText
+                  fullRound
+                  value={products.keyword}
+                  placeholder="cari makanan favoritmu..."
+                  fitContainer
+                  onChange={(e) => {
+                    dispatch(setKeyword(e.target.value));
+                  }}
+                />
+              </div>
+
+              <div className="mb-5 pl-2 flex w-3/3 overflow-auto pb-5">
+                {tags[products.category].map((tag, index) => {
+                  return (
+                    <div key={index}>
+                      <Pill
+                        text={tag}
+                        icon={tag.slice(0, 1).toUpperCase()}
+                        isActive={products.tags.includes(tag)}
+                        onClick={(_) => dispatch(toggleTag(tag))}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+
+              {products.status === "process" && !products.data.length ? (
+                <div className="flex justify-center">
+                  <BounceLoader color="red" />
+                </div>
+              ) : null}
+
+              <Responsive desktop={3} items="stretch">
+                {products.data.map((product, index) => {
+                  return (
+                    <div key={index} className="p-2">
+                      <CardProduct
+                        title={product.name}
+                        imgUrl={`${config.api_host}/upload/${product.image_url}`}
+                        price={product.price}
+                        onAddToCart={(_) => null}
+                      />
+                    </div>
+                  );
+                })}
+              </Responsive>
+              <div className="text-center my-10">
+                <Pagination
+                  totalItems={products.totalItems}
+                  page={products.currentPage}
+                  perPage={products.perPage}
+                  onChange={(page) => dispatch(setPage(page))}
+                  onNext={(_) => dispatch(goToNextPage())}
+                  onPrev={(_) => dispatch(gotoPrevPage())}
+                />
+              </div>
             </div>
             <div className="w-full md:w-1/4 h-full shadow-lg border-r border-white bg-gray-100">
               Keranjang belanja di sini
